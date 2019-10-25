632332b532c09ca27263ec83848ca7906ff1917a
Danielmg <Danielmg_21@hotmail.es>
2019-10-25 14:30:00 +0200

    Cambios clase EXIT

--- app/src/main/java/com/example/tetris/Exit.java
+++ app/src/main/java/com/example/tetris/Exit.java
@@ -1,27 +1,27 @@
 package com.example.tetris;
 
-
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Exit extends AppCompatActivity {
-
-    Button again;
-    Button exit;
-
+    public Ranking ranking = new Ranking();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exit);
 
-        again = (Button) findViewById(R.id.Again);
-        exit = (Button) findViewById(R.id.exit);
+        Button again = findViewById(R.id.Again);
+        Button exit = findViewById(R.id.exit);
+        Button registrar = findViewById(R.id.registrar_puntuacion);
+        TextView nombre = findViewById(R.id.nombre_jugador);
+        final TextView mostrarRanking = findViewById(R.id.mostrarRanking);
+
 
         again.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,13 +30,21 @@ public class Exit extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        again.setOnClickListener(new View.OnClickListener() {
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ranking.Registrar(view);
+                mostrarRanking.setVisibility(view.VISIBLE);
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+    }
+    public void Registrar(View view){
 
     }
 }
