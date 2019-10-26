d3dc02a07ea0ed8be3df11f05e6083f93e62602b
oscar <oscar.rivas.99@gmail.com>
2019-10-26 22:31:56 +0200

    mostrar top 6 listo

--- app/src/main/java/com/example/tetris/Exit.java
+++ app/src/main/java/com/example/tetris/Exit.java
@@ -16,9 +16,10 @@ import android.widget.Toast;
 public class Exit extends AppCompatActivity {
     private EditText et_nombre;
     private Button registrar;
-    private TextView mostrarRanking;
+    private TextView mostrarRanking1,mostrarRanking2,mostrarRanking3,mostrarRanking4,mostrarRanking5,mostrarRanking6,mostrarPunt1,mostrarPunt2,mostrarPunt3,mostrarPunt4,mostrarPunt5,mostrarPunt6;
     private SQLiteDatabase BaseDeDatos;
     private AdminSQLiteOpenHelper BBDD;
+    private boolean registrado;
 
 
     @Override
@@ -30,9 +31,28 @@ public class Exit extends AppCompatActivity {
         Button exit = findViewById(R.id.exit);
         registrar = findViewById(R.id.registrar_puntuacion);
         TextView nombre = findViewById(R.id.nombre_jugador);
-        mostrarRanking = findViewById(R.id.mostrarRanking1);
+        registrado=false;
+
+        mostrarRanking1 = findViewById(R.id.mostrarRanking1);
+        mostrarRanking2 = findViewById(R.id.mostrarRanking2);
+        mostrarRanking3 = findViewById(R.id.mostrarRanking3);
+        mostrarRanking4 = findViewById(R.id.mostrarRanking4);
+        mostrarRanking5 = findViewById(R.id.mostrarRanking5);
+        mostrarRanking6 = findViewById(R.id.mostrarRanking6);
+
+        mostrarPunt1= findViewById(R.id.textpunt1);
+        mostrarPunt2= findViewById(R.id.textpunt2);
+        mostrarPunt3= findViewById(R.id.textpunt3);
+        mostrarPunt4= findViewById(R.id.textpunt4);
+        mostrarPunt5= findViewById(R.id.textpunt5);
+        mostrarPunt6= findViewById(R.id.textpunt6);
+
+
+
 
         et_nombre = (EditText)findViewById(R.id.nombre_jugador);
+
+        //mostrarTop5();
 
         again.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,9 +65,19 @@ public class Exit extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //restablecerEstadiaticas();
+
+                /*
+                mostrarRanking1.setVisibility(view.VISIBLE);
+                mostrarRanking2.setVisibility(view.VISIBLE);
+                mostrarRanking3.setVisibility(view.VISIBLE);
+                mostrarRanking4.setVisibility(view.VISIBLE);
+                mostrarRanking5.setVisibility(view.VISIBLE);
+                mostrarRanking6.setVisibility(view.VISIBLE);
+                */
+
                 Registrar(view);
-                mostrarRanking.setVisibility(view.VISIBLE);
-                mostrarTop5(view);
+                mostrarTop5();
+
             }
         });
         exit.setOnClickListener(new View.OnClickListener() {
@@ -63,12 +93,12 @@ public class Exit extends AppCompatActivity {
         et_nombre = (EditText)findViewById(R.id.nombre_jugador);
         String nombre = et_nombre.getText().toString();
 
-        if ( !nombre.isEmpty()){
+        if (!nombre.isEmpty() & !registrado){
             //permite almacenar las columnas del registro en pares clave-valor
             ContentValues registro = new ContentValues();
             //Añade los pares
             registro.put("nombre", nombre);
-            registro.put("puntuacion", 20);
+            registro.put("puntuacion", 60);
 
             //insertar valores en la tabla ranking
             BaseDeDatos.insert("rankingNormal", null, registro);
@@ -76,16 +106,23 @@ public class Exit extends AppCompatActivity {
             BaseDeDatos.close();
 
             et_nombre.setText("");
-
+            registrado=true;
             Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
 
         }else{
-            Toast.makeText(this, "Debes rellenar el nombre", Toast.LENGTH_SHORT).show();
+            if (nombre.isEmpty()) {
+                Toast.makeText(this, "Debes rellenar el nombre", Toast.LENGTH_SHORT).show();
+            }
+            if (registrado){
+                Toast.makeText(this, "Ya te has registrado", Toast.LENGTH_SHORT).show();
+            }
         }
     }
-    public void mostrarTop5 (View view){
-        BaseDeDatos = BBDD.getWritableDatabase();
+    public void mostrarTop5 (){
+        BaseDeDatos = BBDD.getReadableDatabase();
         String columnas[] = new String[]{"nombre","puntuacion"};//,"puntuacion"
+
+        String j1="",j2="",j3="",j4="",j5="",j6="",p1="",p2="",p3="",p4="",p5="",p6="";
 
         //********************** AMBAS FUNCIONAN
         //-----1 forma
@@ -96,17 +133,65 @@ public class Exit extends AppCompatActivity {
         if(fila1.moveToFirst()){
             //mostrarRanking.setText(fila1.getString(0)+fila1.getString(1));
 
-            mostrarRanking.setText(fila1.getString(0)+fila1.getString(1));
-            fila1.moveToNext();
-            mostrarRanking.setText(fila1.getString(0)+fila1.getString(1));
-            fila1.moveToNext();
+            j1 = fila1.getString(0);
+            p1 = fila1.getString(1);
 
-
+            if (fila1.moveToNext()) {
+                fila1.moveToNext();
+                j2 = fila1.getString(0);
+                p2 = fila1.getString(1);
+            }
+            if (fila1.moveToNext()) {
+                fila1.moveToNext();
+                j3 = fila1.getString(0);
+                p3 = fila1.getString(1);
+            }
+            if (fila1.moveToNext()) {
+                fila1.moveToNext();
+                j4 = fila1.getString(0);
+                p4 = fila1.getString(1);
+            }
+            if (fila1.moveToNext()) {
+                fila1.moveToNext();
+                j5 = fila1.getString(0);
+                p5 = fila1.getString(1);
+            }
+            if (fila1.moveToNext()) {
+                fila1.moveToNext();
+                j6 = fila1.getString(0);
+                p6= fila1.getString(1);
+            }
 
             Toast.makeText(this, "Muestro", Toast.LENGTH_SHORT).show();
         }else {
-            Toast.makeText(this, "No muestro", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Ranking vacio", Toast.LENGTH_SHORT).show();
+            j1="";
+            j2="";
+            j3="";
+            j4="";
+            j5="";
+            j6="";
+            p1="";
+            p2="";
+            p3="";
+            p4="";
+            p5="";
+            p6="";
         }
+
+        mostrarRanking1.setText(j1);
+        mostrarRanking2.setText(j2);
+        mostrarRanking3.setText(j3);
+        mostrarRanking4.setText(j4);
+        mostrarRanking5.setText(j5);
+        mostrarRanking6.setText(j6);
+
+        mostrarPunt1.setText(p1);
+        mostrarPunt2.setText(p2);
+        mostrarPunt3.setText(p3);
+        mostrarPunt4.setText(p4);
+        mostrarPunt5.setText(p5);
+        mostrarPunt6.setText(p6);
 
         BaseDeDatos.close();
     }
@@ -129,6 +214,8 @@ public class Exit extends AppCompatActivity {
         */
         Toast.makeText(this, "Estadisticas restablecidas", Toast.LENGTH_SHORT).show();
         BaseDeDatos.close();
+
+        mostrarTop5();
     }
 
 
